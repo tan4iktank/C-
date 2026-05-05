81e1d20d33c36cd30a3acabe32c2bde5d80dd9c1
Oleg <malimon61423@gmail.com>
2026-05-05 23:04:03 +0500

    add lesso1:task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,8 +8,28 @@
 // Инициализировать папку Lesson1,  ввести в терминале (git init) //
 // Добавить файл "Task1", ввести в терминале (git add Task1) //
 // Сохранить изменения в файле, ввести в терминале (git commit -m "add lesson1:task1") //
-// Создать оригинальный репозиторий, в GitHub создать новый репозиторий, ввести в терминале (git remote add origine https://github.com/tan4iktank/c-.git) //
+// Создать оригинальный репозиторий, в GitHub создать новый репозиторий, ввести в терминале (git remote add origin https://github.com/tan4iktank/c-.git) //
 // Отправить изменения в удаленный репозиторий, ввести в терминале (git push origine main) //
+/* Работа с кодом
+
+int value =100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char   c = '!';
+bool   b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);    */
+// Ввести в терминале (dotnet run), получить результат. //
+// Ввести в терминале (git add Program.cs)//
+// Закомментировать изменения, вести в терминале (git commit -m "add some variables lesson1:task1")
+// Отправить на удалённый репозиторий, ввести в терминале (git push origine main) //
+// Переход в папку "Lesson1", ввести в терминале (cd ..) //
+// Создать папку "Task2", ввести в терминале (mkdir Task2) //
+// Переход в папку "Task2", ввести в терминале (cd Task2) //
+
+   
 
 // Console.WriteLine("Hello World"); //
 
